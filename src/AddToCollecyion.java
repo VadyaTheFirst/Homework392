eabd67d3c5a336aa2abfc444a0dc8f41aa80153e
vadym <vadikhide@yandex.ru>
2022-11-27 21:17:04 +0200

    Домашка 1

---
+++ src/AddToCollecyion.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class AddToCollecyion {
+
+    public static void AddMethod (HashMap<String,Integer> map, String string, Integer integer)throws RuntimeException{
+        if(map.containsKey(string) && map.get(string).equals(integer)){
+          throw new RuntimeException("Такой ключ: "+string+" и значение: "+integer+" уже есть в массиве");
+        } else if (map.containsValue(integer)){
+            map.replace(string,integer);
+        } else{
+            map.put(string,integer);
+        }
+   }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Map<String,Integer> collection =new  HashMap<>();
+        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"a",1);
+        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"b",2);
+        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"a",3);
+        System.out.println(collection);
+
     }
 }
