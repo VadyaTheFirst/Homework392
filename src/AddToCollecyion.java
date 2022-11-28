b150aa4f6261cdedebb48651c4c48428caf31586
vadym <vadikhide@yandex.ru>
2022-11-28 13:50:37 +0200

    Домашка 1 изменил название с учетом camelCase

--- src/AddToCollecyion.java
+++ src/AddToCollecyion.java
@@ -2,13 +2,13 @@ import java.util.HashMap;
 
 public class AddToCollecyion {
 
-    public static void AddMethod (HashMap<String,Integer> map, String string, Integer integer)throws RuntimeException{
-        if(map.containsKey(string) && map.get(string).equals(integer)){
-          throw new RuntimeException("Такой ключ: "+string+" и значение: "+integer+" уже есть в массиве");
-        } else if (map.containsValue(integer)){
-            map.replace(string,integer);
-        } else{
-            map.put(string,integer);
+    public static void addMethod(HashMap<String, Integer> map, String string, Integer integer) throws RuntimeException {
+        if (map.containsKey(string) && map.get(string).equals(integer)) {
+            throw new RuntimeException("Такой ключ: " + string + " и значение: " + integer + " уже есть в массиве");
+        } else if (map.containsValue(integer)) {
+            map.replace(string, integer);
+        } else {
+            map.put(string, integer);
         }
-   }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Map<String,Integer> collection =new  HashMap<>();
-        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"a",1);
-        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"b",2);
-        AddToCollecyion.AddMethod((HashMap<String, Integer>) collection,"a",3);
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"a",1);
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"b",2);
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"a",3);
         System.out.println(collection);
 
     }
