b0dcf4d940d82cd46e32f20fcbc6e43b42b125fb
vadym <vadikhide@yandex.ru>
2022-11-28 13:50:58 +0200

    Домашка 1 изменил название с учетом camelCase

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String,Integer> collection =new  HashMap<>();
-        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"a",1);
-        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"b",2);
-        AddToCollecyion.addMethod((HashMap<String, Integer>) collection,"a",3);
+        Map<String, Integer> collection = new HashMap<>();
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection, "a", 1);
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection, "b", 2);
+        AddToCollecyion.addMethod((HashMap<String, Integer>) collection, "a", 3);
         System.out.println(collection);
 
     }
